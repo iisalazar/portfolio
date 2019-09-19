9544c7f53a1c8ef0be1b0678917b199b113c616c
= <=>
2019-09-19 11:15:19 +0800

    Initial commit: Had to rush work because class is almost over

---
+++ src/components/LandingBanner.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="hero is-dark is-medium">
+    <div class="hero-body">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <h1 class="title">
+              Hero title
+            </h1>
+            <h2 class="subtitle">
+              Hero subtitle
+            </h2>
+          </div>
+          <div class="column">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+  export default {
+    name: "LandingBanner"
+  }
+</script>
+<style scoped>
+.banner {
+  background: url("../assets/banner.jpg") no-repeat center;
+  background-size: cover;
+	background-position: 50%;
+	height: 640px;
+	width: 100%;
+
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <LandingBanner/>
+  </div>
+</template>
+
+<script>
+import LandingBanner from '@/components/LandingBanner.vue'
+export default {
+  name: 'home',
+  components: {
+    LandingBanner
+  }
+}
+</script>
