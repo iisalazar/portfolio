851077b8883e70c0513bf4a42abf00f3d66671e1
= <=>
2019-09-19 12:02:53 +0800

    Initial commit: Had to rush work because class is almost over

--- src/components/LandingBanner.vue
+++ src/components/LandingBanner.vue
@@ -1,23 +1,16 @@
 <template>
-  <section class="hero is-dark is-medium">
-    <div class="hero-body">
-      <div class="container">
-        <div class="columns">
-          <div class="column">
-            <h1 class="title">
-              Hero title
-            </h1>
-            <h2 class="subtitle">
-              Hero subtitle
-            </h2>
-          </div>
-          <div class="column">
-            
-          </div>
+  <div class="banner">
+    <div class="bg">
+      <div class="columns content is-desktop">
+        <div class="column">
+          HELLO
+        </div>
+        <div class="column">
+          HELLO
         </div>
       </div>
     </div>
-  </section>
+  </div>
 </template>
 <script>
   export default {
@@ -28,9 +21,19 @@
 .banner {
   background: url("../assets/banner.jpg") no-repeat center;
   background-size: cover;
-	background-position: 50%;
+	background-position: 100%;
 	height: 640px;
 	width: 100%;
+}
 
+.banner .content {
+  padding: 15%;
+  color: white;
+}
+
+.banner .bg {
+  background-color: rgba(0, 0, 0, 0.6);
+  width: 100%;
+  height: 100%;
 }
 </style>
